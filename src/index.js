8e724042be2b315c1dc949a73aa79aaf6103f9f7
Pavel <66474636+Comet-Z@users.noreply.github.com>
2021-05-23 16:03:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // === Import React: === //
 import React from 'react';
 import ReactDOM from 'react-dom';
-// === Import styles: === //
-import './index.css';
+
 // === Import custom components === //
 import App from './Components/App.js';
 
