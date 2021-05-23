3f85cd066560043f970f336f6b547fde0402ddb6
Pavel <66474636+Comet-Z@users.noreply.github.com>
2021-05-23 15:52:32 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// === Import React: === //
+import React from 'react';
+import ReactDOM from 'react-dom';
+// === Import styles: === //
+import './index.css';
+// === Import custom components === //
+import App from './Components/App.js';
+
+ReactDOM.render(<App />, document.getElementById("root"))
